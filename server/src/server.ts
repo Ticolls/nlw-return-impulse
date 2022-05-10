4707264cba43a6004efe4d0b211424d13b0f86e1
Thiago Seixas <ticolls@hotmail.com>
2022-05-10 18:45:23 -0300

    mais uma

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     //Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With')
     app.use(cors());
     next();
 });
