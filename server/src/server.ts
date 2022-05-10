cab4a47f24335a21e33acaa3b26bd975317f50fc
Thiago Seixas <ticolls@hotmail.com>
2022-05-10 19:38:59 -0300

    .........

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,12 +5,8 @@ import { routes } from './routes'
 
 const app = express()
 
-app.options('/login', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
-});
+app.use(cors())
+
 app.use(express.json())
 app.use(routes)
 
