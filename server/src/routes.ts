240534cc89703f48b850e556d13552a2d2c8deb7
Thiago Seixas <ticolls@hotmail.com>
2022-05-10 18:50:20 -0300

    .

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import cors from 'cors'
 
 import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter';
 import { SubmitFeedback } from './functions/submit-feedback';
