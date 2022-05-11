a6ca01a6eedc9f7ff5d33c6e0fe8266f79972004
Thiago Seixas <ticolls@hotmail.com>
2022-05-10 22:35:19 -0300

    adicionando o email

--- server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -14,7 +14,7 @@ export class NodemailerMailAdapter implements mailAdapter {
     async sendMail({ subject, body }: sendMailData) {
         await transport.sendMail({
             from: "Equipe Feedget <oi@feedget.com>",
-            to: "Thiago Seixas <ticolls@hotmail.com>",
+            to: "ticolls@hotmail.com",
             subject,
             html: body,
         })
